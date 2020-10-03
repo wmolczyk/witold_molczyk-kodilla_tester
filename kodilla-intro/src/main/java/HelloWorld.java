8efcd45662d777f65b5b73aa0c91b0e4818f17ca
Witek Molczyk <witold_molczyk@op.pl>
2020-10-03 21:21:54 +0200

    Version2

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args ) {
         System.out.println("Hello World");
         System.out.println("Witek");
-        System.out.println(10);
+        System.out.println(40);
         System.out.println('M');
            }
             }
