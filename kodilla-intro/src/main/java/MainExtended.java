5f1d5b36462cc8dee2e45437884d3273107797fa
Witek Molczyk <witold_molczyk@op.pl>
2020-10-03 22:27:43 +0200

    Version3

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,14 @@
+public class MainExtended {public static void main(String[] args){
+    String carModel = "Fiesta";
+    String carMaker = "Ford";
+    int engineCapacity = 1250;
+    char version = 'G';
+    boolean secondHand = true;
+    double pricePLN = 30000.67;
+    System.out.println(engineCapacity);
+    System.out.println(carMaker);
+    System.out.println(carModel);
+    System.out.println(secondHand);
+    System.out.println(version);
+}
+}
